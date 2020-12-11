4fbe1ed04fdefaee68c27405f4a8be6082366da8
Mohamed Nedal Abomokh <45313255+Mohamab29@users.noreply.github.com>
2020-12-11 21:11:44 +0200

    finished preparing the training part

--- main.py
+++ main.py
@@ -1,26 +1,16 @@
-from sklearn.model_selection import train_test_split
-from tensorflow.python.keras.utils.np_utils import to_categorical
-
 from model import unet
-from data import *
+from preprocessing import *
 import matplotlib.pyplot as plt
 from keras.models import load_model
 import cv2
 
 
-# load an image and predict the class
-def run_example():
-    # load the image
-    img = load_image('C:/Users/Vladis/Desktop/cell_images/Uninfected/C241NThinF_IMG_20151207_124643_cell_125.png')
-    # load model
-    model = load_model('unet.h5')
-    # predict the class
-    result = model.predict(img)
-    print('Prediction:', result[0])
-
-
 # plot diagnostic learning curves
 def summarize_diagnostics(history):
+    """
+    printing the summary of of model
+    :arg history:the model and it's history basically
+    """
     f, (ax1, ax2) = plt.subplots(1, 2, figsize=(12, 4))
     t = f.suptitle('CNN Performance', fontsize=12)
     f.subplots_adjust(top=0.85, wspace=0.3)
@@ -44,32 +34,32 @@ def summarize_diagnostics(history):
     l2 = ax2.legend(loc="best")
 
 
-def load_image(filename):
-    # load the image
-    img = load_img(filename, color_mode='rgb', target_size=(SIZE, SIZE))
-    # convert to np.array
-    img = np.array(img)
-    # reshape into a single sample with 3 channels
-    img = img.reshape(1, img.shape[0], img.shape[1], img.shape[2])
-    return img
-
-
-def train():
-    model = unet()
-    dataset, label = create_dataset_and_labels('data/membrane/train/', 'image/', 'label/')
-    X_train, X_test, y_train, y_test = train_test_split(np.asarray(dataset), np.asarray(label), test_size=0.20)
+def train(no_of_iters):
+    """
+    we first load the Unet model and then create the training data that we want to fit to our model
+    and print the summary
+    """
+    start_time = time()
+    print("loading the model")
+    model = unet(input_size=(desired_size, desired_size, 1))
+    print("preparing the data for training")
+    x_train, y_train = prepare2train(no_of_iters)
     print(model.summary())
     history = model.fit(
-        x=X_train,
+        x=x_train,
         y=y_train,
-        batch_size=10,
+        batch_size=100,
         verbose=1,
-        epochs=5,
+        epochs=15,
         validation_split=0.1,
     )
-    # print("Test_Accuracy: {:.2f}%".format(model.evaluate(np.array(X_test), np.array(y_test))[1] * 100))
-    # summarize_diagnostics(history)
-    # model.save('unet.h5')
+    print_time(s_time=start_time, msg="done training the U-net model")
+    print("saving the model")
+    model.save('Unet.h5')
+
+    summarize_diagnostics(history)
+    print_time(s_time=start_time, msg="finished running the script")
 
 
-train()
+if __name__ == "__main__":
+    train(40)
